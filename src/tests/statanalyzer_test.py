2ffd5a349b66cff0a35c7c8d3211719f42d4a8a2
toppyy <topias.pyykkonen@helsinki.fi>
2020-11-05 18:25:48 +0200

    setup tests

---
+++ src/tests/statanalyzer_test.py
@@ -0,0 +1,16 @@
+import unittest
+from ..StatAnalyzer import StatAnalyzer
+
+
+testdata = {
+    "column": [40.8 , 21.7 , 50 , 67 , 42 , 35.5 , 90.7 , 62.1 , 74.7 , 39.5 , 56.7 , 47.2 , 61.7 , 52.2 , 68.9 , 53.8 , 81.7 , 37.1 , 48.6 , 61.5]
+}
+
+
+class TestStatAnalyzer(unittest.TestCase):
+
+    def setUp(self):
+        self.StatAnalyzer = StatAnalyzer(testdata)
+
+    def test_mean(self):
+        self.assertEqual( self.StatAnalyzer.mean("column"), 54.67 )
