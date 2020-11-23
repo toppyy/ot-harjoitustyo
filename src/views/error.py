50e57ebff075b0a9436cc071b1139caa38fea914
toppyy <topias.pyykkonen@helsinki.fi>
2020-11-23 19:11:45 +0200

    generic error-view

---
+++ src/views/error.py
@@ -0,0 +1,19 @@
+from tkinter import ttk, Tk
+
+
+class Error:
+
+    def __init__(self,error_msg):
+        self.error_msg = error_msg
+
+    def throw(self):
+
+        window = Tk()
+        window.title("Analysis setup")
+        
+        self.frame = ttk.Frame( master=window)
+
+        label = ttk.Label(master=self.frame,text=self.error_msg)
+        label.grid(row=0,column=0)
+
+        self.frame.pack()
