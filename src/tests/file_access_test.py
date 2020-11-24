13fe9376563243f05747f99209fad46f54b3e91c
toppyy <topias.pyykkonen@helsinki.fi>
2020-11-24 18:49:06 +0200

    more tests

---
+++ src/tests/file_access_test.py
@@ -0,0 +1,14 @@
+import unittest
+from file_access import FileAccess
+
+
+class TestGuesstype(unittest.TestCase):
+
+    def test_csv_read(self):
+
+        path = './data/sepelvaltimo_korvausoikeus.csv'
+        rows = FileAccess().read_csv(path, ";", '"')
+
+        first_row = 'aluekoodi;maakunta;vaesto65_84;sepelvaltimotauti_korv_oikeus;sepelvaltimotauti_korv_oikeus_osuus'
+        
+        self.assertEqual(first_row,';'.join(rows[0]))
--- src/tests/statanalyzer_test.py
+++ src/tests/statanalyzer_test.py
@@ -3,27 +3,46 @@ from stat_analyzer import StatAnalyzer
 from dataset import Dataset
 
 
-testcolumn = [40.8, 21.7, 50, 67, 42, 35.5, 90.7, 62.1, 74.7,
-              39.5, 56.7, 47.2, 61.7, 52.2, 68.9, 53.8, 81.7, 37.1, 48.6, 61.5]
-testdata = [[str(x)] for x in testcolumn]
-
-testdataset = Dataset(testdata)
-
-testdataset.create(has_header=False)
-
+# Helper to create data for testing
+def letter(i):
+       
+    if i % 2 == 0:
+        return 'A'
+    return 'B'
 
 class TestStatAnalyzer(unittest.TestCase):
 
     def setUp(self):
+        testcolumn = [40.8, 21.7, 50, 67, 42, 35.5, 90.7, 62.1, 74.7,
+              39.5, 56.7, 47.2, 61.7, 52.2, 68.9, 53.8, 81.7, 37.1, 48.6, 61.5]
+
+        
+        testdata = [[str(val),letter(i)] for i,val in enumerate(testcolumn)]
+        testdataset = Dataset(testdata)
+        testdataset.create(has_header=False)
+
         self.stat_analyzer = StatAnalyzer(testdataset)
+
+        
+
 
     def test_summary(self):
 
         summaryresult = self.stat_analyzer.summary("col0")
 
-        mean = list(filter(lambda r: r['text'] == 'Mean: ', summaryresult))[0]
-        median = list(
-            filter(lambda r: r['text'] == 'Median: ', summaryresult))[0]
+        mean   = list(filter(lambda r: r['id'] == 'mean', summaryresult))[0]
+        median = list(filter(lambda r: r['id'] == 'median', summaryresult))[0]
+        stdev  = list(filter(lambda r: r['id'] == 'stddev', summaryresult))[0]
 
         self.assertEqual(mean["value"], 54.67)
         self.assertEqual(median["value"], 53.8)
+        self.assertAlmostEqual(stdev["value"],16.87019,places=5)
+
+    def test_frequency_table(self):
+
+        freqtable = self.stat_analyzer.frequencytable("col1")
+
+        numberOfAs = freqtable[0]
+
+        # TODO: better way to test this
+        self.assertEqual(numberOfAs['value'][0:5],'A: 10')
