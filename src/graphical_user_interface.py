cdbb8c68cf846134d876b51ae00b650da4a0e71e
toppyy <topias.pyykkonen@helsinki.fi>
2020-11-24 17:24:45 +0200

    refactor Commands out

--- src/graphical_user_interface.py
+++ src/graphical_user_interface.py
@@ -1,15 +1,24 @@
 from misc.init_setup import init_setup
 from views.commands import Commands
-
+from tkinter import ttk
 class GUI:
 
     def __init__(self, root, StatAnalyzer):
         self.root = root
         self.stat_analyzer = StatAnalyzer
+        
 
     def start(self):
-        view = Commands(self.root, self.do_command, self.stat_analyzer.get_available_commands())
-        view.pack()
+        frame = ttk.Frame(master=self.root)
+
+        available_commands = self.stat_analyzer.get_available_commands()
+
+        for idx, text in enumerate(available_commands):
+            btn = ttk.Button(master=frame, text=text,
+                             command=lambda text=text: self.do_command(text))
+            btn.grid(row=idx+1, column=0)
+
+        frame.pack()
 
     def do_command(self, analysis_name):
 
